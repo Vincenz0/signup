f06abd9f744b0581efbc7651d2919982c6581a12
Tomi <tkluka@gmail.com>
2020-01-26 13:44:56 +0100

    first commit

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="This is an example of a meta description. This will often show up in search results.">
+    <meta name=viewport content="width=device-width, inital-scale=1">
+    <title></title>
+    </head>
+    <body>
+      <header>
+        <nav>
+          <a href="#">
+            <img src="img/logo.png" alt="logo">
+          </a>
+          <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="">Portfolio</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Me 9:37</a></li>
+          </ul>
+        </nav>
+      </header>
+    </body>
+</html>
